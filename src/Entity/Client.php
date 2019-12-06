294bcde00d1311864e261822a915877e1525709e
Justinas Garipovas <justinas.garipovas@gmail.com>
2019-12-07 00:14:25 +0200

    feat: Added user entities

---
+++ src/Entity/Client.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ClientRepository")
+ */
+class Client
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $username;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $password_encoded;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): self
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    public function getPasswordEncoded(): ?string
+    {
+        return $this->password_encoded;
+    }
+
+    public function setPasswordEncoded(string $password_encoded): self
+    {
+        $this->password_encoded = $password_encoded;
+
+        return $this;
+    }
+}
